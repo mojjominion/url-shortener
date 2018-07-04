53ab764b488e4b4166b4fc98c7d61f37e04c28e2
mohan <lmohan717@gmail.com>
2018-07-04 18:24:13 +0530

    nrkfn

--- src/settings/local.py
+++ src/settings/local.py
@@ -1,5 +1,6 @@
 from .base import *
 
+SECRET_KEY = '6_*pxtdfv!&9yhhy+nn@s)_9)^+!htjjbe$n%b+6f42)4z*yn2'
 
 SECURE_SSL_REDIRECT = False
 
--- src/settings/production.py
+++ src/settings/production.py
@@ -12,5 +12,6 @@ DEFAULT_REDIRECT_URL     = 'https://mojjo.herokuapp.com'
 PARENT_HOST              = 'mojjo.herokuapp.com'
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = '6_*pxtdfv!&9yhhy+nn@s)_9)^+!htjjbe$n%b+6f42)4z*yn2'
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
